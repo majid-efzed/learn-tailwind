81d4eea55da764f20884253401558664256e10d2
majid-efzed <042909831@ecampus.ut.ac.id>
2022-11-08 14:21:53 +0700

    add extend theme animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
         'press-start': ['"Press Start 2P"', 'cursive'],
         'poppins': ['Poppins', 'sans-serif'],
       },
+      animation: {
+        'goyang': 'goyang 1s ease-in-out infinite',
+        'fast-pulse': 'pulse .5s ease-in infinite'
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        },
+      },
     },
   },
   plugins: [],
